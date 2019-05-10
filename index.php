f1af9f919a2d4607d335784960cd4c7fdc3eba70
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-05-10 23:03:35 +0100

    new-look

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Water Polo League</title>
 <meta name="keywords" content="water,polo,league,table,stats,goals,points,east,midlands,championship,oundle,bishop,stortford,harrow,stamford"/>
 <meta name="description" content="Home of the East Midlands Independent Schools Water Polo Leage"/>

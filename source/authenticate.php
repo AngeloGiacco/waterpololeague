a72e62f50f18c62dd311fe6f4be309db1121f7cc
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-06-13 15:03:44 +0100

    test switch case decision and add buttons for usertype + refactor code

---
+++ source/authenticate.php
@@ -0,0 +1,53 @@
+<?php
+  array_map("htmlspecialchars", $_POST);
+  include_once("connection.php");
+  try {
+    $userType = $_POST["userType"];
+    $email = $_POST["email"];
+    $attempt = $_POST["pswd"];
+    switch ($userType) {
+      case "admin":
+        echo "admin";
+        break;
+      case "coach":
+        echo "coach";
+        break;
+      case "player":
+        echo "player";
+        break;
+    }
+    if ($_POST["uname"] == "admin" and $_POST["psw"] == "nimda") {// if user login information correct
+      $_SESSION["admin"] = true;
+      header("Location: updateOptions.php");
+      exit();
+    } else {
+      $stmt = $conn->prepare("SELECT password FROM pupils WHERE email = :email");
+      $stmt->bindParam(':email',$_POST["uname"]);
+      $stmt->execute();
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      $attempt = $_POST["psw"];
+      $hashed = $row["password"];
+      if (password_verify($attempt,$hashed)){
+        $_SESSION["email"] = $_POST["uname"];
+        header("Location: order.php");
+        exit();
+      } else {
+        ?><script>
+          if (window.confirm('Incorrect password, please press ok to try again')){
+            window.location.href='index.php?login=false';
+          } else {
+  					window.location.href='index.php?login=false';
+  				};
+          </script><?php
+          exit();
+      }
+    }
+    $conn=null;
+  }
+  $conn=null;
+}
+catch(PDOException $e)
+  	{
+  		echo "error".$e->getMessage();
+  	}
+?>

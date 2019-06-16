133f1884661d72f7c14c630fa489d549ca1e0276
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-06-16 11:11:24 +0100

    send email with contact message

--- source/contactUs.php
+++ source/contactUs.php
@@ -1,6 +1,92 @@
 <?php
-  $message = $_POST["message"];
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  echo $message ." from ". $name ." who's email is ".$email;
+  if (isset($_POST["message"]) and $_POST["name"] and$_POST["name"] and $_POST["subject"]) {
+    $message = $_POST["message"];
+    $name = $_POST["name"];
+    $email =$_POST["name"];
+    $subject = $_POST["subject"];
+    $txt = "'".$message. "' sent from ".$email;
+    $website_email = "contact@emiswaterpolo.dx.am";
+    $headers = "From: ".$website_email. "\r\n";
+    $to = "giacco.angelo.s@gmail.com";
+    mail($to,$subject,$txt,$headers);
+  } else {
+    ?><script>
+        alert("invalid information sent");
+        window.location.replace("contact.html");
+      </script><?php
+  }
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>EMIS Water Polo</title>
+
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link href="styles/template.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a style = "color: #fbc93d" class="navbar-brand" href="#">EMIS Water Polo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="table.html">View League Table</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="stats.html">View Statistics</a>
+            </li>
+          </ul>
+          <a href = "login.html"><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button></a>
+        </div>
+      </nav>
+    </header>
+
+    <main role="main" height = "100%">
+      <div height = "50rem" id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="img-fluid" src="styles/images/waterpolo.jpg" alt="Water Polo">
+            <div class="container">
+              <div class="carousel-caption text-left" height="40rem">
+                <h1>Message sent!</h1>
+                <p>Thanks for getting touch with us!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+			<footer class="container">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>Made by &copy;AngeloGiacco
+        <a style = "font-size: 40px; margin-right: 20px; margin-left:10px" href="https://twitter.com/giaccoangelo" target="_blank"><i class="fa fa-twitter"></i></a>
+        <a style = "font-size: 40px" href="https://www.linkedin.com/in/angelo-giacco-450b2017a/" target="_blank"><i class="fa fa-linkedin"></i></a></p>
+        <p>Share This:
+        <a style = "font-size: 40px; margin-right: 20px; margin-left: 10px;" href="https://twitter.com/home?status=Completely%20free,%20modern,%20water%20polo%20leageue%20management%20website%20by%20@giaccoangelo" target="_blank"><i class="fa fa-twitter"></i></a>
+        <a style = "font-size: 40px; margin-right: 20px;" href="https://www.facebook.com/sharer/sharer.php?u=https://github.com/AngeloGiacco/waterpololeaguecoursework" target="_blank"><i class="fa fa-facebook"></i></a>
+        <a style = "font-size: 40px; margin-right: 20px;"
+        href="https://www.reddit.com/submit?title=Completely%20free,%20modern,%20water%20polo%20leageue%20management%20website&url=emiswaterpololeague.herokuapp.com" target="_blank"><i class="fa fa-reddit"></i></a>
+        <a style = "font-size: 40px" href="https://www.linkedin.com/shareArticle?mini=true&url=emiswaterpololeague.herokuapp.com&title=Completely%20free,%20modern,%20water%20polo%20leageue%20management%20website" target="_blank"><i class="fa fa-linkedin"></i></a></p>
+      </footer>
+		</main>
+
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  </body>
+</html>

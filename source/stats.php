2b57126ed17ae149f543d939303359e8b4769c91
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-09-03 11:55:41 +0100

    create search function

--- source/stats.php
+++ source/stats.php
@@ -35,16 +35,17 @@
     </script>
     <script>
     function myFunction() {
-      var input, filter, table, tr, td, i, playername,teamname;
+      var input, filter, table, tr, td, i, txtValue;
       input = document.getElementById("myInput");
       filter = input.value.toUpperCase();
       table = document.getElementById("5");
       tr = table.getElementsByTagName("tr");
+
       for (i = 0; i < tr.length; i++) {
-        playername = tr[i].getElementsByTagName("td")[0].innerText;
-        teamname = tr[i].getElementsByTagName("td")[1].innerText;
-        if (playername && teamname) {
-          if (playername.toUpperCase().indexOf(filter) > -1 || teamname.toUpperCase().indexOf(filter) > -1) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
             tr[i].style.display = "";
           } else {
             tr[i].style.display = "none";
